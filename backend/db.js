62e2a0fc33184ee8f822893dcdcc31dbf5ccb832
Takalanimbodi <133506394+Takalanimbodi@users.noreply.github.com>
2026-03-10 22:17:03 +0200

    db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Ntsako@21#',  // your MySQL password
+  password: ' ',  // your MySQL password
   database: 'lms'
 });
 
