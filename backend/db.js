076b2ed7249a1ea3ab67e53ed8e8507281fd27c3
Takalanimbodi <133506394+Takalanimbodi@users.noreply.github.com>
2026-03-16 23:40:31 +0200

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,19 @@
-import mysql from "mysql2"
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: ' ',  // your MySQL password
-  database: ' '
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL connected!");
+    if (err) throw err;
+    console.log("MySQL connected!");
 });
 
 export default db;
-
 
